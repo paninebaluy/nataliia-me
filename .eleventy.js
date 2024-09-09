dfdc0880982d022a7e711c56992f51ed40240d3a
Natalia Dolgushina <paninebaluy@Air-de-Natalia.lan>
2024-09-09 20:49:04 +0100

    add box animation

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     // This will copy these folders to the output without modifying them at all
     eleventyConfig.addPassthroughCopy("vendor");
     eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("images");
     return {
         markdownTemplateEngine: "njk",
         htmlTemplateEngine: "pug"
